cea8d4303eb07bd28f62b2095a57c169cc606dda
ac7v4 <73734277+ac7v4@users.noreply.github.com>
2021-10-23 23:48:51 -0400

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,7 +8,7 @@
 sed -i 's/192.168.1.1/10.4.16.247/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+#sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
